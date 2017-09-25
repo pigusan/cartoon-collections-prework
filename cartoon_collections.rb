2338f619f49aa7ec05ee80568f69e25758d46119
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 22:15:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,7 @@ def long_planeteer_calls(array)
   end
 end
 
+=begin
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |types|
@@ -28,6 +29,7 @@ def find_the_cheese(array)
   end
   nil
 end
+=end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
