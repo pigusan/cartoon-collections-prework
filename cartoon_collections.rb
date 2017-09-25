455d5b53d179a852c293b7dfedd30ee8b565bb87
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 22:17:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |ingredient| 
+  array.find do |ingredient|
     cheese_types.include?(ingredient)
   end
 end
